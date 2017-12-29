a85b664c02f4a043c1b1ae51a6ffc8b4cee2ebb7
AvetisTG <avetis.tergrigoryan@gmail.com>
2017-12-29 18:02:34 +0400

    not responsive commit 2

--- src/App.js
+++ src/App.js
@@ -3,10 +3,6 @@ import { connect } from 'react-redux';
 import Header from './components/header'
 import Subheader from './components/subHeader'
 
-@connect((store) => {
-    return {
-    }
-})
 class App extends Component {
 
 
